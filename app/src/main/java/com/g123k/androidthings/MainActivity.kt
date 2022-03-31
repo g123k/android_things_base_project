ceb357eddcce8f36ca8414759e04efd15710863b
Edouard Marquez <moi@edouard-marquez.me>
2022-03-31 15:03:17 +0200

    first commit

---
+++ app/src/main/java/com/g123k/androidthings/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.g123k.androidthings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+  }
+}
