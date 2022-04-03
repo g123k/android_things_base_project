e6ff8268095a48d5aa7f8c16154309133ac349f4
Edouard Marquez <g123k@users.noreply.github.com>
2022-04-03 19:45:49 +0200

    Support latest AS version

--- app/src/main/java/com/g123k/androidthings/MainActivity.kt
+++ app/src/main/java/com/g123k/androidthings/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.g123k.androidthings
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    setContentView(TextView(this).apply {
+      val localIpAddress = Utils.getLocalIpAddress()
+      text = localIpAddress
+      Log.d("MYIP", localIpAddress)
+    })
   }
 
   override fun onDestroy() {
---
+++ app/src/main/java/com/g123k/androidthings/Utils.java
@@ -0,0 +1,28 @@
+package com.g123k.androidthings;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class Utils {
+
+    public static String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+}
